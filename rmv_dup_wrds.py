58620f5ac57dd7c69e7bd76d30e9ccad470897c3
DLamarG <demondg8@gmail.com>
2024-09-09 10:31:26 -0500

    added correct test results for each test

--- rmv_dup_wrds.py
+++ rmv_dup_wrds.py
@@ -14,6 +14,6 @@ def remove_duplicate_words(s):
 
 
 
-print(remove_duplicate_words("my cat is my cat fat"))
-print(remove_duplicate_words("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"))
+print(remove_duplicate_words("my cat is my cat fat")) # "my cat is fat"
+print(remove_duplicate_words("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta")) # "alpha beta gamma delta"
 print(remove_duplicate_words("alpha beta beta gamma gamma gamma"))
